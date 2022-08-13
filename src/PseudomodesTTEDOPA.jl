b8c50004ecd6ce40128bc59056aece26e1d3dcce
Davide Ferracin <davide.ferracin@protonmail.com>
2022-08-13 19:10:10 +0200

    Add existing source files
    The "lib" files from the repository used for the master's thesis,
    containing the common definition and functions used in all the
    simulation scripts, were added here as the main package code.

--- src/PseudomodesTTEDOPA.jl
+++ src/PseudomodesTTEDOPA.jl
@@ -1,5 +1,23 @@
 module PseudomodesTTEDOPA
 
-# Write your package code here.
+using Base.Filesystem
+using CSV
+using Combinatorics
+using DataFrames
+using ITensors
+using JSON
+using LinearAlgebra
+using Measures
+using Plots
+using PolyChaos
+using ProgressMeter
+using QuadGK
+
+include("utils.jl")
+include("plotting.jl")
+include("spin_chain_space.jl")
+include("harmonic_oscillator_space.jl")
+include("operators.jl")
+include("tedopa.jl")
 
 end
