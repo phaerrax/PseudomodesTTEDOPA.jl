d41975a7e6573d80f8f552a93f0856f34ce01ee0
Davide Ferracin <davide.ferracin@protonmail.com>
2022-08-14 11:15:04 +0200

    Exported functions in module
    The scripts using this package should be able to call these functions
    without prepending the module name (or else all previously written
    simulations would break).

--- src/PseudomodesTTEDOPA.jl
+++ src/PseudomodesTTEDOPA.jl
@@ -13,11 +13,65 @@ using PolyChaos
 using ProgressMeter
 using QuadGK
 
+export allequal,
+       #canonicalbasis,
+       #canonicalmatrix,
+       chain,
+       chop,
+       construct_step_list,
+       disablegrifqtech,
+       embed_slice,
+       #gellmannbasis,
+       #gellmannmatrix,
+       #isjson,
+       levels,
+       linkdims,
+       load_parameters,
+       partialtrace,
+       #sitetypes,
+       #vec,
+       vonneumannentropy
+
 include("utils.jl")
+
+export groupplot,
+       unifiedlogplot,
+       unifiedplot
+
 include("plotting.jl")
+
+export chain_L1_state,
+       chain_basis_states,
+       level_subspace_proj,
+       parse_init_state,
+       parse_spin_state,
+       single_ex_state,
+       spin_current_op_list
+
 include("spin_chain_space.jl")
+
+export mixedlindbladminus,
+       mixedlindbladplus,
+       osc_levels_proj,
+       oscdimensions,
+       parse_init_state_osc
+
 include("harmonic_oscillator_space.jl")
+
+export twositeoperators,
+       localop,
+       interactionop,
+       evolve,
+       current,
+       forwardflux,
+       backwardflux
+
 include("operators.jl")
+
+export getchaincoefficients,
+       thermalisedJ,
+       chainmapcoefficients
+
 include("tedopa.jl")
 
 end
