917ff50def532a0f3cc54940d731efaf22cdc76d
Davide Ferracin <davide.ferracin@protonmail.com>
2023-05-26 13:48:56 +0200

    Add adjoint versions of closure, spin chain etc.

--- src/closure.jl
+++ src/closure.jl
@@ -1,6 +1,7 @@
 export Closure, closure
 export freq, innercoup, outercoup, damp, freqs, innercoups, outercoups, damps
-export closure_op, filled_closure_op
+export closure_op, closure_op_adjoint, closure_op′
+export filled_closure_op, filled_closure_op_adjoint, filled_closure_op′
 
 """
     Closure(ω::Vector{<:Real}, γ::Vector{<:Real}, g::Vector{<:Real}, ζ::Vector{<:Complex})
@@ -77,6 +78,8 @@ innercoup(mc::Closure, j::Int) = mc.innercoupling[j]
 outercoup(mc::Closure, j::Int) = mc.outercoupling[j]
 damp(mc::Closure, j::Int) = mc.damping[j]
 
+############################################################################################
+
 closure_op(::SiteType, ::Closure, ::Vector{<:Index}, ::Int) = nothing
 
 """
@@ -87,7 +90,6 @@ Return an OpSum object encoding the Markovian closure operators with parameters 
 `chain_edge_site`.
 This closure replaces a chain starting from an empty state.
 """
-
 function closure_op(mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int)
     stypes = sitetypes(first(sites))
     for st in stypes
@@ -210,6 +212,162 @@ function closure_op(
     return ℓ
 end
 
+############################################################################################
+
+closure_op_adjoint(::SiteType, ::Closure, ::Vector{<:Index}, ::Int, ::Int) = nothing
+
+"""
+    closure_op_adjoint(
+        mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int, gradefactor::Int
+    )
+
+Return an OpSum object encoding the adjoint of the Markovian closure operators with
+parameters given by `mc`, on sites `sites`, linked to the main TEDOPA/thermofield chain on
+site `chain_edge_site`. The integer `gradefactor` is the parity (1: even, -1: odd) of the
+operator subject to the time evolution.
+This closure replaces a chain starting from an empty state.
+"""
+function closure_op_adjoint(
+    mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int, gradefactor::Int
+)
+    stypes = sitetypes(first(sites))
+    for st in stypes
+        # Check if all sites have this type (otherwise skip this tag).
+        if all(i -> st in sitetypes(i), sites)
+            # If the type is shared, then try calling the function with it.
+            ℓ = closure_op_adjoint(st, mc, sitenumber.(sites), chain_edge_site, gradefactor)
+            # If the result is something, return that result.
+            if !isnothing(ℓ)
+                return ℓ
+            end
+        end
+        # Otherwise, try again with another type from the initial ones.
+    end
+    # Return an error if no implementation is found for any type.
+    return throw(
+        ArgumentError(
+            "Overload of \"closure_op\" function not found for Index tags $(tags(sites[1]))"
+        ),
+    )
+end
+
+function closure_op_adjoint(
+    ::SiteType"vS=1/2",
+    mc::Closure,
+    sites::Vector{<:Index},
+    chain_edge_site::Int,
+    gradefactor::Int,
+)
+    ℓ = OpSum()
+    for (j, site) in enumerate(sitenumber.(sites))
+        ℓ += -freq(mc, j) * gkslcommutator("N", site)
+    end
+    for (j, (site1, site2)) in enumerate(partition(sitenumber.(sites), 2, 1))
+        jws = jwstring(; start=site1, stop=site2)
+        ℓ +=
+            -innercoup(mc, j) * (
+                gkslcommutator("σ-", site1, jws..., "σ+", site2) +
+                gkslcommutator("σ+", site1, jws..., "σ-", site2)
+            )
+    end
+    for (j, site) in enumerate(sitenumber.(sites))
+        jws = jwstring(; start=chain_edge_site, stop=site)
+        ℓ += -(
+            outercoup(mc, j) * gkslcommutator("σ+", chain_edge_site, jws..., "σ-", site) +
+            conj(outercoup(mc, j)) *
+            gkslcommutator("σ-", chain_edge_site, jws..., "σ+", site)
+        )
+    end
+
+    for (j, site) in enumerate(sitenumber.(sites))
+        # a ρ a†
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "σ+⋅ * ⋅σ-"]
+        ℓ += (
+            gradefactor * damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...
+        )
+        # -0.5 (a† a ρ + ρ a† a)
+        ℓ += -0.5damp(mc, j), "N⋅", site
+        ℓ += -0.5damp(mc, j), "⋅N", site
+    end
+    return ℓ
+end
+
+function closure_op_adjoint(
+    ::SiteType"vElectron",
+    mc::Closure,
+    sites::Vector{<:Index},
+    chain_edge_site::Int,
+    gradefactor::Int,
+)
+    ℓ = OpSum()
+    for (j, site) in enumerate(sitenumber.(sites))
+        ℓ += -freq(mc, j) * gkslcommutator("Ntot", site)
+    end
+    for (j, (site1, site2)) in enumerate(partition(sitenumber.(sites), 2, 1))
+        # Add as many "F" string operator as needed.
+        jws = jwstring(; start=site1, stop=site2)
+        ℓ +=
+            -innercoup(mc, j) * (
+                gkslcommutator("Aup†F", site1, jws..., "Aup", site2) -
+                gkslcommutator("AupF", site1, jws..., "Aup†", site2) +
+                gkslcommutator("Adn†", site1, jws..., "FAdn", site2) -
+                gkslcommutator("Adn", site1, jws..., "FAdn†", site2)
+            )
+    end
+    for (j, site) in enumerate(sitenumber.(sites))
+        # c↑ᵢ† c↑ᵢ₊ₙ = a↑ᵢ† Fᵢ Fᵢ₊₁ ⋯ Fᵢ₊ₙ₋₁ a↑ᵢ₊ₙ
+        # c↑ᵢ₊ₙ† c↑ᵢ = -a↑ᵢ Fᵢ Fᵢ₊₁ ⋯ Fᵢ₊ₙ₋₁ a↑ᵢ₊ₙ†
+        # c↓ᵢ† c↓ᵢ₊ₙ = a↓ᵢ† Fᵢ₊₁ Fᵢ₊₂ ⋯ Fᵢ₊ₙ a↓ᵢ₊ₙ
+        # c↓ᵢ₊ₙ† c↓ᵢ = -a↓ᵢ Fᵢ₊₁ Fᵢ₊₂ ⋯ Fᵢ₊ₙ a↓ᵢ₊ₙ†
+
+        jws = jwstring(; start=chain_edge_site, stop=site)
+        # ζⱼ c↑₀† c↑ⱼ (0 = chain edge, j = pseudomode)
+        ℓ +=
+            -outercoup(mc, j) *
+            gkslcommutator("Aup†F", chain_edge_site, jws..., "Aup", site)
+        # conj(ζⱼ) c↑ⱼ† c↑₀
+        ℓ +=
+            conj(outercoup(mc, j)) *
+            gkslcommutator("AupF", chain_edge_site, jws..., "Aup†", site)
+        # ζⱼ c↓₀† c↓ⱼ
+        ℓ +=
+            -outercoup(mc, j) *
+            gkslcommutator("Adn†", chain_edge_site, jws..., "FAdn", site)
+        # conj(ζⱼ) c↓ⱼ† c↓₀
+        ℓ +=
+            conj(outercoup(mc, j)) *
+            gkslcommutator("Adn", chain_edge_site, jws..., "FAdn†", site)
+    end
+
+    # Dissipative part
+    for (j, site) in enumerate(sitenumber.(sites))
+        # Remember that:
+        # • Fⱼ = (1 - 2 N↑ₖ) (1 - 2 N↓ₖ);
+        # • Fⱼ and aₛ,ₖ commute only on different sites;
+        # • {a↓ₖ, a↓ₖ†} = {a↑ₖ, a↑ₖ†} = 1;
+        # • Fₖ anticommutes with a↓ₖ, a↓ₖ†, a↑ₖ and a↑ₖ†.
+
+        # c↑ₖ† X c↑ₖ = a↑ₖ† Fₖ₋₁ ⋯ F₁ X F₁ ⋯ Fₖ₋₁ a↑ₖ
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "Aup†⋅ * ⋅Aup"]
+        ℓ += (gradefactor * damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
+        # c↓ₖ† X c↓ₖ = a↓ₖ†Fₖ Fₖ₋₁ ⋯ F₁ X F₁ ⋯ Fₖ₋₁ Fₖa↓ₖ
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "Adn†F⋅ * ⋅FAdn"]
+        ℓ += (gradefactor * damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
+
+        # -½ (c↑ₖ† c↑ₖ ρ + ρ c↑ₖ† c↑ₖ) = -½ (a↑ₖ† a↑ₖ ρ + ρ a↑ₖ† a↑ₖ)
+        ℓ += -0.5damp(mc, j), "Nup⋅", site
+        ℓ += -0.5damp(mc, j), "⋅Nup", site
+        # -½ (c↓ₖ† c↓ₖ ρ + ρ c↓ₖ† c↓ₖ) = -½ (a↓ₖ† a↓ₖ ρ + ρ a↓ₖ† a↓ₖ)
+        ℓ += -0.5damp(mc, j), "Ndn⋅", site
+        ℓ += -0.5damp(mc, j), "⋅Ndn", site
+    end
+    return ℓ
+end
+
+const closure_op′ = closure_op_adjoint
+
+############################################################################################
+
 filled_closure_op(::SiteType, ::Closure, ::Vector{<:Index}, ::Int) = nothing
 
 """
@@ -268,7 +426,7 @@ function filled_closure_op(
     end
 
     for (j, site) in enumerate(sitenumbers)
-        # a† ρ a
+        # a ρ a†
         opstring = [repeat(["F⋅ * ⋅F"], site - 1); "σ+⋅ * ⋅σ-"]
         ℓ += (damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
         # -0.5 (a a† ρ + ρ a a†)
@@ -347,3 +505,149 @@ function filled_closure_op(
     end
     return ℓ
 end
+
+############################################################################################
+
+filled_closure_op_adjoint(::SiteType, ::Closure, ::Vector{<:Index}, ::Int, ::Int) = nothing
+
+"""
+    filled_closure_op_adjoint(
+        mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int, gradefactor::Int
+    )
+
+Return an OpSum object encoding the Markovian closure operators with parameters given by
+`mc`, on sites `sites`, linked to the main TEDOPA/thermofield chain on site
+`chain_edge_site`. The integer `gradefactor` is the parity (1: even, -1: odd) of the
+operator subject to the time evolution.
+This closure replaces a chain starting from a completely filled state.
+"""
+function filled_closure_op_adjoint(mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int, gradefactor::Int)
+    stypes = sitetypes(first(sites))
+    for st in stypes
+        # Check if all sites have this type (otherwise skip this tag).
+        if all(i -> st in sitetypes(i), sites)
+            # If the type is shared, then try calling the function with it.
+            ℓ = filled_closure_op_adjoint(st, mc, sitenumber.(sites), chain_edge_site, gradefactor)
+            # If the result is something, return that result.
+            if !isnothing(ℓ)
+                return ℓ
+            end
+        end
+        # Otherwise, try again with another type from the initial ones.
+    end
+    # Return an error if no implementation is found for any type.
+    return throw(
+        ArgumentError(
+            "Overload of \"filled_closure_op\" function not found for " *
+            "Index tags $(tags(sites[1]))",
+        ),
+    )
+end
+
+function filled_closure_op_adjoint(
+    ::SiteType"vS=1/2", mc::Closure, sitenumbers::Vector{<:Int}, chain_edge_site::Int, gradefactor::Int
+)
+    ℓ = OpSum()
+    for (j, site) in enumerate(sitenumbers)
+        ℓ += -freq(mc, j) * gkslcommutator("N", site)
+    end
+    for (j, (site1, site2)) in enumerate(partition(sitenumbers, 2, 1))
+        jws = jwstring(; start=site1, stop=site2)
+        ℓ +=
+            -innercoup(mc, j) * (
+                gkslcommutator("σ-", site1, jws..., "σ+", site2) +
+                gkslcommutator("σ+", site1, jws..., "σ-", site2)
+            )
+    end
+    for (j, site) in enumerate(sitenumbers)
+        jws = jwstring(; start=chain_edge_site, stop=site)
+        ℓ += -(
+            outercoup(mc, j) * gkslcommutator("σ+", chain_edge_site, jws..., "σ-", site) +
+            conj(outercoup(mc, j)) *
+            gkslcommutator("σ-", chain_edge_site, jws..., "σ+", site)
+        )
+    end
+
+    for (j, site) in enumerate(sitenumbers)
+        # a ρ a†
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "σ-⋅ * ⋅σ+"]
+        ℓ += (damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
+        # -0.5 (a a† ρ + ρ a a†) = -0.5 (ρ - a† a ρ + ρ a† a)
+        ℓ += 0.5damp(mc, j), "N⋅", site
+        ℓ += 0.5damp(mc, j), "⋅N", site
+        ℓ += -damp(mc, j), "Id", site
+    end
+    return ℓ
+end
+
+function filled_closure_op_adjoint(
+    ::SiteType"vElectron", mc::Closure, sites::Vector{<:Index}, chain_edge_site::Int, gradefactor::Int
+)
+    ℓ = OpSum()
+    for (j, site) in enumerate(sitenumber.(sites))
+        ℓ += -freq(mc, j) * gkslcommutator("Ntot", site)
+    end
+    for (j, (site1, site2)) in enumerate(partition(sitenumber.(sites), 2, 1))
+        # Add as many "F" string operator as needed.
+        jws = jwstring(; start=site1, stop=site2)
+        ℓ +=
+            -innercoup(mc, j) * (
+                gkslcommutator("Aup†F", site1, jws..., "Aup", site2) -
+                gkslcommutator("AupF", site1, jws..., "Aup†", site2) +
+                gkslcommutator("Adn†", site1, jws..., "FAdn", site2) -
+                gkslcommutator("Adn", site1, jws..., "FAdn†", site2)
+            )
+    end
+    for (j, site) in enumerate(sitenumber.(sites))
+        jws = jwstring(; start=chain_edge_site, stop=site)
+        # ζⱼ c↑₀† c↑ⱼ (0 = chain edge, j = pseudomode)
+        ℓ +=
+            -outercoup(mc, j) * gkslcommutator("Aup†F", chain_edge_site, jws..., "Aup", site)
+        # conj(ζⱼ) c↑ⱼ† c↑₀
+        ℓ +=
+            conj(outercoup(mc, j)) *
+            gkslcommutator("AupF", chain_edge_site, jws..., "Aup†", site)
+        # ζⱼ c↓₀† c↓ⱼ
+        ℓ +=
+            -outercoup(mc, j) * gkslcommutator("Adn†", chain_edge_site, jws..., "FAdn", site)
+        # conj(ζⱼ) c↓ⱼ† c↓₀
+        ℓ +=
+            conj(outercoup(mc, j)) *
+            gkslcommutator("Adn", chain_edge_site, jws..., "FAdn†", site)
+    end
+
+    # Dissipative part
+    for (j, site) in enumerate(sitenumber.(sites))
+        # Remember that:
+        # • Fⱼ = (1 - 2 N↑ₖ) (1 - 2 N↓ₖ);
+        # • Fⱼ and aₛ,ₖ commute only on different sites;
+        # • {a↓ₖ, a↓ₖ†} = {a↑ₖ, a↑ₖ†} = 1;
+        # • Fₖ anticommutes with a↓ₖ, a↓ₖ†, a↑ₖ and a↑ₖ†.
+        #
+        # c↑ₖ X c↑ₖ† = F₁ ⋯ Fₖ₋₁ a↑ₖ X a↑ₖ† Fₖ₋₁ ⋯ F₁
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "Aup⋅ * ⋅Aup†"]
+        ℓ += (gradefactor * damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
+        # c↓ₖ X c↓ₖ† = F₁ ⋯ Fₖ₋₁ Fₖa↓ₖ X a↓ₖ†Fₖ Fₖ₋₁ ⋯ F₁
+        opstring = [repeat(["F⋅ * ⋅F"], site - 1); "FAdn⋅ * ⋅Adn†F"]
+        ℓ += (gradefactor * damp(mc, j), collect(Iterators.flatten(zip(opstring, 1:site)))...)
+
+        # c↑ₖ c↑ₖ† = F₁ ⋯ Fₖ₋₁ a↑ₖ a↑ₖ† Fₖ₋₁ ⋯ F₁ =
+        #          = F₁² ⋯ Fₖ₋₁² a↑ₖ a↑ₖ† =
+        #          = a↑ₖ a↑ₖ† =
+        #          = 1 - N↑ₖ
+        ℓ += -damp(mc, j), "Id", site
+        ℓ += 0.5damp(mc, j), "Nup⋅", site
+        ℓ += 0.5damp(mc, j), "⋅Nup", site
+        # c↓ₖ c↓ₖ† = F₁ ⋯ Fₖ₋₁ Fₖa↓ₖ a↓ₖ†Fₖ Fₖ₋₁ ⋯ F₁ =
+        #          = F₁² ⋯ Fₖ₋₁² Fₖa↓ₖ a↓ₖ†Fₖ =
+        #          = Fₖa↓ₖ a↓ₖ†Fₖ =
+        #          = Fₖ² a↓ₖ a↓ₖ† =
+        #          = 1 - N↓ₖ
+        ℓ += -damp(mc, j), "Id", site
+        ℓ += 0.5damp(mc, j), "Ndn⋅", site
+        ℓ += 0.5damp(mc, j), "⋅Ndn", site
+    end
+    return ℓ
+end
+
+const filled_closure_op′ = filled_closure_op_adjoint
--- src/spin_chain.jl
+++ src/spin_chain.jl
@@ -1,4 +1,5 @@
-export exchange_interaction, spin_chain
+export spin_chain, spin_chain_adjoint, spin_chain′
+export exchange_interaction, exchange_interaction_adjoint, exchange_interaction′
 
 spin_chain(::SiteType, ::Vector{<:Real}, ::Vector{<:Real}, ::Vector{<:Int}) = nothing
 
@@ -75,6 +76,71 @@ function spin_chain(
     return ℓ
 end
 
+############################################################################################
+
+function spin_chain_adjoint(::SiteType, ::Vector{<:Real}, ::Vector{<:Real}, ::Vector{<:Int})
+    return nothing
+end
+
+"""
+    spin_chain_adjoint(
+        freqs::Vector{<:Real},
+        coups::Vector{<:Real},
+        sites::Vector{<:Index}
+    )
+
+Return an OpSum object encoding the adjoint of the Hamiltonian part ``-i[H, –]`` of the
+GKSL equation for a spin chain of frequencies `freqs` and coupling constants `coups`, on
+`sites`.
+"""
+function spin_chain_adjoint(
+    freqs::Vector{<:Real}, coups::Vector{<:Real}, sites::Vector{<:Index}
+)
+    stypes = sitetypes(first(sites))
+    for st in stypes
+        # Check if all sites have this type (otherwise skip this tag).
+        if all(i -> st in sitetypes(i), sites)
+            # If the type is shared, then try calling the function with it.
+            ℓ = spin_chain_adjoint(st, freqs, coups, sitenumber.(sites))
+            # If the result is something, return that result.
+            if !isnothing(ℓ)
+                return ℓ
+            end
+        end
+        # Otherwise, try again with another type from the initial ones.
+    end
+    # Return an error if no implementation is found for any type.
+    return throw(
+        ArgumentError(
+            "Overload of \"spin_chain_adjoint\" function not found for " *
+            "Index tags $(tags(sites[1]))",
+        ),
+    )
+end
+
+# In the GKSL equation, the adjoint of -i[H,-] is simply i[H,-].
+function spin_chain_adjoint(
+    st::SiteType"vS=1/2",
+    freqs::Vector{<:Real},
+    coups::Vector{<:Real},
+    sitenumbers::Vector{<:Int},
+)
+    return -spin_chain(st, freqs, coups, sitenumbers)
+end
+
+function spin_chain_adjoint(
+    st::SiteType"vElectron",
+    freqs::Vector{<:Real},
+    coups::Vector{<:Real},
+    sitenumbers::Vector{<:Int},
+)
+    return -spin_chain(st, freqs, coups, sitenumbers)
+end
+
+const spin_chain′ = spin_chain_adjoint
+
+############################################################################################
+
 exchange_interaction(::SiteType, ::Int, ::Int) = nothing
 
 """
@@ -130,3 +196,48 @@ function exchange_interaction(::SiteType"vElectron", site1::Int, site2::Int)
     )
     return ℓ
 end
+
+############################################################################################
+
+exchange_interaction_adjoint(::SiteType, ::Int, ::Int) = nothing
+
+"""
+    exchange_interaction_adjoint(s1::Index, s2::Index)
+
+Return an OpSum object encoding the adjoint of the Hamiltonian part ``-i[H, –]`` of an
+exchange interaction between sites `s1` and `s2` term in a GKSL equation.
+"""
+function exchange_interaction_adjoint(s1::Index, s2::Index)
+    stypes1 = sitetypes(s1)
+    stypes2 = sitetypes(s2)
+    for st in stypes1
+        # Check if all sites have this type (otherwise skip this tag).
+        if st in stypes2
+            # If the type is shared, then try calling the function with it.
+            ℓ = exchange_interaction_adjoint(st, sitenumber(s1), sitenumber(s2))
+            # If the result is something, return that result.
+            if !isnothing(ℓ)
+                return ℓ
+            end
+        end
+        # Otherwise, try again with another type from the initial ones.
+    end
+    # Return an error if no implementation is found for any type.
+    return throw(
+        ArgumentError(
+            "Overload of \"exchange_interaction_adjoint\" function not found for " *
+            "Index tags $(tags(s1))",
+        ),
+    )
+end
+
+# In the GKSL equation, the adjoint of -i[H,-] is simply i[H,-].
+function exchange_interaction_adjoint(st::SiteType"vS=1/2", site1::Int, site2::Int)
+    return -exchange_interaction(st, site1, site2)
+end
+
+function exchange_interaction_adjoint(st::SiteType"vElectron", site1::Int, site2::Int)
+    return -exchange_interaction(st, site1, site2)
+end
+
+const exchange_interaction′ = exchange_interaction_adjoint
