9a2f0ab4bc4511405fbdedff254595121683500b
Davide Ferracin <davide.ferracin@protonmail.com>
2023-11-06 14:30:53 +0100

    Add `ntot^2` operator to the `Electron` site type
    This is not necessary for an `Electron`-type Index, since it can be
    easily built by multiplying two `ntot` ITensors operators, but for
    `vElectron` types, since it comes in handy when the `ntot^2` is needed
    inside a `gkslcommutator` function.

--- src/site_types/electron.jl
+++ src/site_types/electron.jl
@@ -3,3 +3,6 @@
 function ITensors.op(::OpName"NupNdn", st::SiteType"Electron")
     return ITensors.op(OpName("Nup"), st) * ITensors.op(OpName("Ndn"), st)
 end
+
+ITensors.op(::OpName"Ntot^2", st::SiteType"Electron") = ITensors.op(OpName("Ntot"), st)^2
+ITensors.op(::OpName"ntot^2", st::SiteType"Electron") = ITensors.op(OpName("Ntot^2"), st)
