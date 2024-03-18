972be0c053d3c204af25269bd6487cfbe555de78
Davide Ferracin <davide.ferracin@protonmail.com>
2024-03-18 02:06:17 +0100

    Deprecate package -- final release
    Set an upper limit to the Julia allowed versions in the project, to
    restrict its use in the future; print a deprecation warning during
    precompilation.

--- src/PseudomodesTTEDOPA.jl
+++ src/PseudomodesTTEDOPA.jl
@@ -96,4 +96,18 @@ export mixedlindbladminus, mixedlindbladplus
 
 include("deprecated.jl")
 
+@warn """
+This package was deprecated on 18 March 2024.
+Its features have been split in three different packages:
+
+* TEDOPA (https://github.com/phaerrax/TEDOPA.jl) for the chain-mapping
+  functionality;
+* LindbladVectorizedTensors (https://github.com/phaerrax/LindbladVectorizedTensors.jl)
+  for the ITensors site type definitions and functions;
+* MarkovianClosure (https://github.com/phaerrax/MarkovianClosure.jl) for the
+  Markovian closure constructions.
+
+This package will remain available, but it won't receive updates anymore.
+"""
+
 end
